dc31d327d2dd28e8e02a3f15b129f066a1dd3480
Yogesh31550 <69802512+Yogesh31550@users.noreply.github.com>
2020-10-17 18:43:48 +0530

    Add files via upload

---
+++ DAY 2/question 1/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        employee e = new employee();
+        employee e1 = new employee();
+
+        e.name="Yogesh";
+        e1.name="Gattu";
+        e.age=21;
+        e1.age=22;
+        e.city="Pune";
+        e1.city="Mumbai";
+        e.display();
+        e1.display();
+
+
+    }
+
+
+
+}
---
+++ DAY 2/question 1/employee.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class employee {
+
+    String name;
+    int age;
+    String city;
+
+    public void display(){
+        System.out.println("The name is  "+name);
+        System.out.println("The age is  "+age);
+        System.out.println("The city is  "+city);
+    }
+
+
+}
